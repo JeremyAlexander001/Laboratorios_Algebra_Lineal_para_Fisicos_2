cae0fe0dc0feb410edf92560cf49223bb4bf8e98
Jeremy Alexander <jeremy.chancahuana.t@uni.pe>
2025-09-15 23:24:40 -0500

    agregé las primeras lineas que mencionó el profesor

--- prueba1.py
+++ prueba1.py
@@ -1,1 +1,12 @@
-print("Hola grupo, no vamos a jalar")
+#! /usr/bin/python3
+from numpy import *
+import matplotlib.pyplot as plt # from pylab import plot,show
+import warnings
+warnings.filterwarnings("ignore")
+
+#primeras 5 lineas, con condiciones del profesor
+
+print ("Hola Mundo")
+
+x=sin(2*pi)
+print (x)
